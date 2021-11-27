319143d60978daac2640d2d46f18291249bf86c0
Oscar Calderon <gregoriocalderon.07@hotmail.es>
2021-11-27 03:51:37 -0400

    sequelize

--- services/product.service.js
+++ services/product.service.js
@@ -1,13 +1,11 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
-const pool = require('../libs/postgres.pool');
+const sequelize = require('../libs/sequelize');
 
 class ProductsService {
   constructor() {
     this.products = [];
     this.generate();
-    this.pool = pool;
-    this.pool.on('error', (e) => console.log(e));
   }
 
   generate() {
@@ -34,8 +32,8 @@ class ProductsService {
 
   async find() {
     const query = 'SELECT * FROM tasks';
-    const rta = await this.pool.query(query);
-    return rta.rows;
+    const [data] = await sequelize.query(query);
+    return data;
   }
 
   async findOne(id) {
