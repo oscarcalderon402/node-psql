597359a92b7ba32afc1ee0b88ed673816167e87f
Oscar Calderon <gregoriocalderon.07@hotmail.es>
2021-12-11 02:40:25 -0400

    relation one to one

---
+++ services/customer.service.js
@@ -0,0 +1,38 @@
+const boom = require('@hapi/boom');
+const { models } = require('../libs/sequelize');
+
+class CustomerService {
+  constructor() {}
+
+  async create(data) {
+    const newCustomer = await models.Customer.create(data);
+    return newCustomer;
+  }
+
+  async find() {
+    const rta = await models.Customer.findAll();
+    return rta;
+  }
+
+  async findOne(id) {
+    const Customer = await models.Customer.findByPk(id);
+    if (!Customer) {
+      throw boom.notFound('Customer not found');
+    }
+    return Customer;
+  }
+
+  async update(id, changes) {
+    const Customer = await this.findOne(id);
+    const rta = await Customer.update(changes);
+    return rta;
+  }
+
+  async delete(id) {
+    const Customer = await this.findOne(id);
+    await Customer.destroy();
+    return { id };
+  }
+}
+
+module.exports = CustomerService;
