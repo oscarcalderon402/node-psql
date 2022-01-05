589f2bd532562cf95fda6581b3bc7e068b9d084d
Oscar Calderon <gregoriocalderon.07@hotmail.es>
2022-01-05 01:25:03 -0500

    has many

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -4,21 +4,24 @@ const id = Joi.string().uuid();
 const name = Joi.string().min(3).max(15);
 const price = Joi.number().integer().min(10);
 const image = Joi.string().uri();
+const description = Joi.string().min(10);
 
 const createProductSchema = Joi.object({
   name: name.required(),
   price: price.required(),
-  image: image.required()
+  description: description.required(),
+  image: image.required(),
 });
 
 const updateProductSchema = Joi.object({
   name: name,
   price: price,
-  image: image
+  image: image,
+  description,
 });
 
 const getProductSchema = Joi.object({
   id: id.required(),
 });
 
-module.exports = { createProductSchema, updateProductSchema, getProductSchema }
+module.exports = { createProductSchema, updateProductSchema, getProductSchema };
