c8f0c01b6a4900c2a8ac498b1c5c631f1387acee
Oscar Calderon <gregoriocalderon.07@hotmail.es>
2022-04-16 14:37:01 -0400

    order

--- services/order.service.js
+++ services/order.service.js
@@ -1,32 +1,39 @@
 const boom = require('@hapi/boom');
+const { models } = require('../libs/sequelize');
 
 class OrderService {
-
-  constructor(){
-  }
+  constructor() {}
   async create(data) {
-    return data;
+    const newOrder = await models.Order.create(data);
+    return newOrder;
   }
 
   async find() {
-    return [];
+    const order = await models.Order.findAll();
+    return order;
   }
 
   async findOne(id) {
-    return { id };
+    const order = await models.Order.findByPk(id, {
+      include: [{ association: 'customer', include: ['user'] }],
+    });
+    if (!order) {
+      throw boom.notFound('Customer not found');
+    }
+    return order;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const Order = await this.findOne(id);
+    const rta = await Order.update(changes);
+    return rta;
   }
 
   async delete(id) {
+    const Order = await this.findOne(id);
+    await Order.destroy();
     return { id };
   }
-
 }
 
 module.exports = OrderService;
